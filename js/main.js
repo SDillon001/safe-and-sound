f994fa33d64e7d7af83a7d4762c4f1f13e509e64
Steve Dillon <stevedillon001@gmail.com>
2021-01-08 10:42:12 -0800

    added quantity interaction

--- js/main.js
+++ js/main.js
@@ -9,13 +9,29 @@ $("flexContainer").ready(function() {
 })
 
 // Drinks qty and order panel
-$('div.drink-container').click(function() {
-    $('div.qty-icon-drink', this).toggleClass('show hide');
-    $('div.add-order-panel').toggleClass('up down');
-});
+// $('div.drink-container').click(function() {
+//     $('div.qty-icon-drink', this).toggleClass('show hide');
+//     $('div.add-order-panel').toggleClass('up down');
+// });
 
 // Food qty and order panel
-$('div.food-container').click(function() {
-    $('div.qty-icon', this).toggleClass('show hide');
-    $('div.add-order-panel').toggleClass('up down');
+// $('div.food-container').click(function() {
+//     $('div.qty-icon', this).toggleClass('show hide');
+//     $('div.add-order-panel').toggleClass('up down');
+// });
+
+// Update food quantity
+$( function() {
+    $('.food-container').click( function() {
+        var num = $(this).find('.qty-icon');
+        num.text( parseInt(num.text()) + 1 );
+    });
+});
+
+// Update drink quantity
+$( function() {
+    $('.drink-container').click( function() {
+        var num = $(this).find('.qty-icon-drink');
+        num.text( parseInt(num.text()) + 1 );
+    });
 });
