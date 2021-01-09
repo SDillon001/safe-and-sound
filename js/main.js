f0122a12781d2d2f10cc4ccdf326a3d996dd7aee
Steve Dillon <stevedillon001@gmail.com>
2021-01-08 19:26:55 -0800

    updated confirm panel

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@ $("flexContainer").ready(function() {
 })
 
 // Drinks qty and order panel
-// $('div.drink-container').click(function() {
-//     $('div.qty-icon-drink', this).toggleClass('show hide');
-//     $('div.add-order-panel').toggleClass('up down');
-// });
+$('div.drink-container').click(function() {
+    $('div.qty-icon-drink', this).toggleClass('show hide');
+    $('div.add-order-panel').toggleClass('up down');
+});
 
 // Food qty and order panel
 // $('div.food-container').click(function() {
