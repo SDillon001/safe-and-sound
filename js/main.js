1a3b3a38607819f7a3d9f5516f1b29ecc40b6759
Steve Dillon <stevedillon001@gmail.com>
2021-01-09 13:51:27 -0800

    updated order panels, user profile

--- js/main.js
+++ js/main.js
@@ -20,33 +20,57 @@ $("flexContainer").ready(function() {
 //     $('div.add-order-panel').toggleClass('up down');
 // });
 
-// Update food quantity
-$(function() {
-    $('.food-container').click( function() {
-        var price = 0;
-        var price_pizza = 5;
-
-        // var num = +($(this).find('.qty-icon').text());
-        // num.text( parseInt(num.text()) + 1 );
-        // console.log("num"+num);
-
-        var num = $(this).find('.qty-icon');
-        num.text( parseInt(num.text()) + 1 );
-        console.log(num);
-
-        var price = price + price_pizza;
-        document.getElementById("total").innerHTML = "$" + price;
-        console.log(price);
-
-        var qty = 1;
-        document.getElementById("items").innerHTML = qty;
-    });
-});
-
 // Update drink quantity
 $(function() {
     $('.drink-container').click( function() {
         var num = $(this).find('.qty-icon-drink');
         num.text( parseInt(num.text()) + 1 );
     });
+});
+
+// Update food quantity
+// $(function() {
+//     $('.food-container').click( function() {
+//         var price = 0;
+//         var price_pizza = 5;
+
+//         // var num = +($(this).find('.qty-icon').text());
+//         // num.text( parseInt(num.text()) + 1 );
+//         // console.log("num"+num);
+
+//         var num = $(this).find('.qty-icon');
+//         num.text( parseInt(num.text()) + 1 );
+//         console.log(num);
+
+//         var price = price + price_pizza;
+//         document.getElementById("total").innerHTML = "$" + price;
+//         console.log(price);
+
+//         var qty = 1;
+//         document.getElementById("items").innerHTML = qty;
+//     });
+// });
+
+// Update food quantity solution from SO - https://stackoverflow.com/questions/65635399/update-incremented-quantity-in-order-item-total/65643408#65643408
+
+$(function() {
+  $('.food-container .qty-icon').click(function() {
+    var num = $(this);
+    num.text(parseInt(num.text()) + 1);
+    var qty = 0;
+    var total = 0;
+    
+    //loop through your food conatiner divs
+    $(".food-container").each(function() {
+      //add total qty on evry iteration
+      qty += parseInt($(this).find(".qty-icon").text())
+      //get actual qty
+      var qty_of_item = parseInt($(this).find(".qty-icon").text())
+      //mutliply it with price add total
+      total += parseInt($(this).find('.price').text().replace('$', '')) * qty_of_item;
+    })
+    
+    $(".order-but-container.one .bold-cash").text(qty); //set new qty
+    $("#total").text("$" + total) //set total
+  });
 });
